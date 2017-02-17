c1c3d66b1aabe429456b3ff6c9d2ad73de8c7812
Bhupesh Pant <bhupesh.pant@snapdeal.com>
2017-02-17 10:04:14 +0530

    First commit

---
+++ src/main/java/com/tp/ds/TP_DS/TestLinkedList.java
@@ -0,0 +1,172 @@
+package com.tp.ds.TP_DS;
+
+
+public class TestLinkedList {
+
+	public static void main(String...s){
+		LinkedList l = new LinkedList();
+		l.add(5);
+		l.add(1);
+		l.add(2);
+		l.add(33);
+		l.add(3);
+		l.add(3);
+		
+		
+		System.out.println("NUMBER COUNT:"+l.numCount(3));
+		System.out.println("NUMBER COUNT:"+l.numCount(33));
+		System.out.println("NUMBER COUNT:"+l.numCount(333));
+		System.out.println("MIDDLE:"+l.middleValue());
+		System.out.println("SIZE:"+l.getSize());
+		System.out.println("DONE");
+		l.print();
+		
+		l.reverse();
+		System.out.println("++++++++++++++");
+		l.print();
+		
+		l.deleteDuplicate();
+		System.out.println("SIZE:"+l.getSize());
+		l.print();
+		
+		
+		LinkedList l1 = new LinkedList();
+		l1.add(3);
+		//l1.delete(3);
+		System.out.println("SIZE:"+l1.getSize());
+	}
+	
+}
+
+
+ class LinkedList {
+     Node head;
+	 class Node{
+		 int i;
+		 Node next=null;
+		 Node(int i){
+			 this.i = i;
+		 }
+	 }
+	 
+	 
+	 //ADD VALUE
+	 public void add(int i){
+		 Node node = new Node(i);
+		 Node temp=head;
+		 if(head==null){
+			 head = node;
+		 }
+		 else{
+		 while(temp.next!=null){
+			 temp = temp.next;
+		 }
+		 temp.next = node;
+		 }
+		 
+		 
+	 }
+	 
+	 //SIZE
+	 public int getSize(){
+		 int count=0;
+		 Node temp = head;
+		 while(temp!=null){
+			 count++;
+			 temp = temp.next;
+		 }
+		 return count;
+	 }
+	 
+	 //DELETE VALUE FORM LINKEDLIST
+	 public void deleteDuplicate(){
+		 Node temp = head;
+		 
+		 while(temp!=null&&temp.next!=null){
+			 for(Node no=temp.next, pre=temp;no!=null;no=no.next,pre=pre.next){
+				 if(temp.i==no.i){
+					 pre.next = no.next;
+				 }
+				
+			 }
+			 temp =temp.next;
+		 }
+	 }
+	 
+/*	 public void delete(int i){
+		 Node temp = head;
+		while(temp!=null){
+			if(temp.next==null&&temp.i==i){
+				 head = null;
+				 break;
+			 }
+			else{
+			Node pre = temp;
+			Node next = pre.next;
+			
+			if(pre==head){
+				pre.next=null;
+			}
+			}
+		 }
+	 }*/
+	 
+	 
+	 //FIND Middle value in linkedlist
+	 public int middleValue(){
+		 Node temp = head;
+		 int i = this.getSize();
+		 int mid = (i/2)+1;
+		 int j=0;
+		 while(temp!=null&&j<mid-1){
+				 j++;
+				 temp = temp.next;
+		
+		 }
+		 return temp.i;
+		// return -1;
+	 }
+	 
+	 //COUNT REPITATION of a number in linkedlist
+	 public int numCount(int n){
+		 int count=0;
+		 Node temp = head;
+		 while(temp!=null){
+			 if(temp.i==n){
+				 count++;
+			 }
+			 temp = temp.next;
+		 }
+		 return count;
+	 }
+	 
+	 public void reverse(){
+		 Node temp = head;
+		 Node f = temp;
+		// f.next = null;
+		// temp.next = null;
+		 while(temp!=null&&temp.next!=null){
+			 f.next.next = temp;
+			// temp = temp.next;
+		 }
+		 
+		 head = f;
+		 
+	 }
+	 
+/*	 public int nthFromLast(int i){
+		 Node temp = head;
+		 int size = this.getSize();
+		 int j = size - i;
+	 }*/
+	 
+	 //PRINT VALUE
+	 public void print(){
+		// LinkedList l = this;
+		Node temp = head;
+		 while(temp!=null){
+			 System.out.println(temp.i);
+			 temp = temp.next;
+		 }
+	 }
+}
